d812a844c577bdb12b93b032e2e1224465c0b554
AlbertoCarballo <161271994+AlbertoCarballo@users.noreply.github.com>
2024-11-09 19:59:03 -0700

    CRUD Brands terminado, falta cambiar rutas

--- app/BrandsController.php
+++ app/BrandsController.php
@@ -1,27 +1,61 @@
 <?php 
 
-/**
- * 
- */
-class BrandsController 
-{
+session_start();
+var_dump($_POST);
+    if(isset($_POST["action"])){
+        // if (!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
+        //     echo "Error: El token no es válido.";
+        //     exit;
+        // }
+        switch($_POST["action"]){
+            case "add_brand":{
+                $name=$_POST["name"];
+                $slug=$_POST["slug"];
+                $description=$_POST["description"];
+                $productController= new BrandsController();
+                $productController->createBrand($name,$slug,$description);
+                break;
+                
+            }
+            case "update_brand":{
+                if (isset($_GET["id"])){
+                    $id=$_GET["id"];
+                };
+                $name=$_POST["name"];
+                $slug=$_POST["slug"];
+                $description=$_POST["description"];
+                $productController= new BrandsController();
+                $productController->updateBrand($id,$name,$slug,$description);
+                break;
+            }
+            case "delete_brand":{
+                $id=$_POST["id"];
+                
+                $productController= new BrandsController();
+                $productController->deleteBrand($id);
+                break;
+            }
+
+        }
+    };
+class BrandsController {
 	
-	public function get()
+	public function getAllBrands()
 	{
 		$curl = curl_init();  
 
 		curl_setopt_array($curl, array(
-		  CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => '',
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 0,
-		  CURLOPT_FOLLOWLOCATION => true,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => 'GET',
-		  CURLOPT_HTTPHEADER => array(
-		    'Authorization: Bearer '.$_SESSION['user_data']->token
-		  ),
+			CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'GET',
+			CURLOPT_HTTPHEADER => array(
+				'Authorization: Bearer '.$_SESSION['user_data']->token
+			),
 		));
 
 		$response = curl_exec($curl);
@@ -36,6 +70,142 @@ class BrandsController
 			return [];
 		}
 
+	}
+
+	public function getSpecificBrand(){
+		if (isset($_GET["id"])){
+			$id=$_GET["id"];
+		};
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands/'.$id,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'GET',
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['user_data']->token
+		),
+		));
+		
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		$response = json_decode($response);
+
+		if (isset($response->code) && $response->code > 0) {
+			
+			return $response->data;
+
+		}else{
+			return [];
+		}
+	}
+
+	public function createBrand($name,$slug,$description){
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => array(
+			'name' =>$name,
+			'description' => $description,
+			'slug' => $slug
+	),
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['user_data']->token
+		),
+		));
+		
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		$response = json_decode($response);
+		if (isset($response->code) && $response->code > 0) {
+			header("Location: ../pruebas-back/index.php");
+		} else {
+			header("Location: ../index.php?status=error");
+		}
+	}
+
+	public function updateBrand($id,$name,$slug,$description){
+		$postFields = "name=" . urlencode($name) .
+                    "&slug=" . urlencode($slug) .
+                    "&description=" . urlencode($description) .
+                    "&id=" . urlencode($id);
+
+					$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'PUT',
+		CURLOPT_POSTFIELDS => $postFields,
+		CURLOPT_HTTPHEADER => array(
+			'Content-Type: application/x-www-form-urlencoded',
+			'Authorization: Bearer '.$_SESSION['user_data']->token
+
+		),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		$response = json_decode($response);
+		if (isset($response->code) && $response->code > 0) {
+			header("Location: ../pruebas-back/index.php");
+		} else {
+			header("Location: ../index.php?status=error");
+		}
+		
+	}
+
+	public function deleteBrand($id){
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands/'.$id,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'DELETE',
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer '.$_SESSION['user_data']->token
+
+		),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		$response = json_decode($response);
+		if (isset($response->code) && $response->code > 0) {
+			header("Location: ../pruebas-back/index.php");
+		} else {
+			header("Location: ../index.php?status=error");
+		}
 	}
 }
 
